5bd92cbf1706a10e90d52df33c85895f100e5848
Diêgo de Barros <debarrosdiego415@gmail.com>
2024-05-31 11:35:59 -0300

    fix: unused vars

--- src/components/RegisterForm/index.jsx
+++ src/components/RegisterForm/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Button } from "../Button/index";
 import { Input } from "../Input/index";
 import {
@@ -10,8 +10,8 @@ import {
   ButtonContainer,
 } from "./styles";
 import { useForm } from "react-hook-form";
-import _isEqual from "lodash/isEqual";
-import { useNavigate, Link } from "react-router-dom";
+// import _isEqual from "lodash/isEqual";
+import { Link } from "react-router-dom";
 // import { LoginContext } from "../../contexts/LoginContext";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
