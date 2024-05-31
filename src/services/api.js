266127c53666fc8480b91c7d545791d1cf8b1aee
Diêgo de Barros <debarrosdiego415@gmail.com>
2024-05-31 12:40:53 -0300

    fixing

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
-const baseURL = process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:8001';
+const baseURL =
+  process.env.NODE_ENV === "production" ? "/api" : "http://localhost:8001";
 
 export const api = axios.create({
   baseURL: baseURL,
